137ff91fd3bbd07efdc75cd476b669a28c8d256a
Charles de Jager <62996317+CharlesDeJager@users.noreply.github.com>
2026-06-04 13:25:42 +0200

    Fix dataframe call in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         # smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         smoothiefroot_response = sf_df.loc[sf_df['name'] == search_on]
-        st_df = st.dataframe(data=smoothiefroot_response.,use_container_width = True)
+        st_df = st.dataframe(data=smoothiefroot_response,use_container_width = True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
